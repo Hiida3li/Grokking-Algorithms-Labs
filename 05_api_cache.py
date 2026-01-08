838d5daf569ab8d4b3c4a99908b62c3f9ebdb96b
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2026-01-08 23:19:41 +0400

    Implemented API Cache Layer using Hash Maps

---
+++ 05_api_cache.py
@@ -0,0 +1,37 @@
+import time
+
+# Caching is simply a Python Dictionary-Hash Map
+# Key: The URL (e.g., "google.com")
+# Value: The Data (e.g., "<html>...</html>")
+
+cache = {}
+
+# SLOW DATABASE SIMULATION
+def get_data_from_database(url):
+    print("Connecting to database for: {url}...")
+    # Simulate a slow network request or heavy SQL query
+    time.sleep(2)
+    return f"DATA_FOR_{url.upper()}"
+
+# The CACHE PROXY
+def fetch_page(url):
+    # Check the Hash Map
+    if url in cache:
+        print(f"CACHE HIT: Found {url} in memory!")
+        return cache[url]
+    else:
+        print(f"CACHE MISS: {url} not found. Fetching from database...")
+        data = get_data_from_database(url)
+
+        # Save it in Caching
+        cache[url] = data
+        return data
+
+requests = ["facebook.com", "facebook.com", "facebook.com", "google.com", "facebook.com"]
+
+print("SERVER STARTING...")
+
+
+
+
+
