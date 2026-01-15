dc348fdcdb456fd5540f49a0cc039c07bd8fb8be
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2026-01-15 17:03:27 +0400

    Implemented Dijkstra Algorithm for GPS Routing

--- 07_dijkstra_nav.py
+++ 07_dijkstra_nav.py
@@ -43,3 +43,17 @@ parents["fin"] = None
 # Processed list: Keep track of nodes we've already calculated
 processed = []
 
+# This function finds the unprocessed node with the smallest cost
+def find_lowest_cost_node(costs): 
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node # After checking all nodes, return the cheapest unprocessed node
+
+    
+
+        
