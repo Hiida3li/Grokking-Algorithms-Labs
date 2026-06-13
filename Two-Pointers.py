606a80e1cd32d43d5fd81d0e17a7673c1ee1a583
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2026-06-13 08:33:04 +0400

    two pointers interview theory

--- Two-Pointers.py
+++ Two-Pointers.py
@@ -1,9 +1,23 @@
+from sympy import false
+
+
 def twoSum(nums, target):
     left, right = 0, len(nums) - 1
     while left < right:
         current_sum = nums[left] + nums[right]
         if current_sum == target:
             return True
+
+        if current_sum < target:
+            left += 1
+
+        else:
+            right -= 1
+
+
+    return False
+
+
 
 
 
