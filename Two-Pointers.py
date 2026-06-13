b3165ffff4efafdd68b8fd2a07cd94d024812b71
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2026-06-13 08:31:11 +0400

    two pointers interview theory

---
+++ Two-Pointers.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        current_sum = nums[left] + nums[right]
+        if current_sum == target:
+            return True
+
+
+
